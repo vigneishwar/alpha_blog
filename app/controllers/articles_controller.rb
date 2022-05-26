a47eafc006c54f34b8582f163b16f2d53eec488a
vigneishwar <vikki1310@gmail.com>
2022-05-27 00:16:40 +1000

    added validations and flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,4 +10,18 @@ class ArticlesController < ApplicationController # rubocop:disable Style/Documen
   def index
     @articles = Article.all
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = 'Succesfully Created an Article'
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
 end
